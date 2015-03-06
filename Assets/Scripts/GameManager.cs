52806f6e61ef134b93484c2652e2d03eb1e51d1b
Sergio González Polán <sergpolan@gmail.com>
2015-03-06 22:25:15 +0100

    Game Manager
    Script GameManager (singleton)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+		public static GameManager instance = null;
+		public BoardManager boardScript;
+		private int level = 3; //vamos a probar el nivel 3 porque es donde aparecen los enemigos
+
+
+
+		void Awake ()
+		{
+				if (instance == null) {
+						instance = this;
+				} else if (instance != this) {
+						Destroy (gameObject);
+				}
+				DontDestroyOnLoad (gameObject); //para que no se borre entre escenas
+				boardScript = GetComponent<BoardManager> ();
+				InitGame ();
+		}
+
+		void InitGame ()
+		{
+				boardScript.SetupScene (level);
+		}
 	
-	}
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		}
 }
